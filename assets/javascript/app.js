31d0d0d3b32ce698e59398ac1d6ae83df4019127
Keivon Arooji <karooji0@email.cpcc.edu>
2017-10-04 20:25:56 -0400

    updated js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,3 @@
-console.log("JavaScript file enabled");
-
 //Create an array that holds the animals
 var animals = ["Tiger", "Wolf", "Bear", "Lion"];
 
@@ -7,17 +5,28 @@ var animals = ["Tiger", "Wolf", "Bear", "Lion"];
 $("#go").on("click", function(){
     //Store the text into a variable
     var newElement = $("#add-this").val().trim();
-    //Push the variable to the array
-    animals.push(newElement);
-    //re-render the buttons
-    renderButtons();
+    //If the variable meets this condition...
+    //Use indexOf() in place of includes()
+    if (newElement !== "" && !animals.includes(newElement)){
+        //Push the variable to the array
+        animals.push(newElement);
+        //Re-render the buttons
+        renderButtons();  
+    }
+    
 });
+
+//When the primary buttons are clicked...
+
+//Launch the ajax
+
+//Console log the response, learn the API
 
 //Dynamically create buttons for items in the array
 function renderButtons(){
     //Prevent duplicate buttons when adding element and reiterating through the array
     $("#button-section").empty();
-    //Generate a button for each item in the array
+    //Generate a button for each item in the array...
     for (var i = 0; i < animals.length; i++){
         var animorph = $("<button>");
         animorph.addClass("btn btn-primary");
