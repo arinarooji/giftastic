75307c575f658021a97a6b91f66a1c032d5febe2
Keivon Arooji <karooji0@email.cpcc.edu>
2017-10-04 20:07:09 -0400

    updated html, css, js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,28 @@
 console.log("JavaScript file enabled");
+
+//Create an array that holds the animals
+var animals = ["Tiger", "Wolf", "Bear", "Lion"];
+
+//When the Go! button is clicked...
+$("#go").on("click", function(){
+    //Store the text into a variable
+    var newElement = $("#add-this").val().trim();
+    //Push the variable to the array
+    animals.push(newElement);
+    //re-render the buttons
+    renderButtons();
+});
+
+//Dynamically create buttons for items in the array
+function renderButtons(){
+    //Prevent duplicate buttons when adding element and reiterating through the array
+    $("#button-section").empty();
+    //Generate a button for each item in the array
+    for (var i = 0; i < animals.length; i++){
+        var animorph = $("<button>");
+        animorph.addClass("btn btn-primary");
+        animorph.text(animals[i]);
+        $("#button-section").append(animorph);
+    }
+}
+renderButtons();
